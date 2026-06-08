e90e4480768c364403ef910f1561116384c28aaf
tyholland <tyronehus@gmail.com>
2026-06-07 19:56:27 -0500

    sign up input fields

--- src/content/sign-up/sign-up.tsx
+++ src/content/sign-up/sign-up.tsx
@@ -3,18 +3,27 @@
 import { userAtom } from "@/caches/UserAtom";
 import { initFirebase } from "@/functions/firebase";
 import { trackError, trackIdentity } from "@/functions/mixpanel";
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  updateProfile,
+} from "firebase/auth";
 import { useSetAtom } from "jotai";
 import Link from "next/link";
+import { ChangeEvent, useState } from "react";
 
 const SignUp = () => {
   initFirebase();
   const auth = getAuth();
   const setUser = useSetAtom(userAtom);
+  const [firstName, setFirstName] = useState<string>("");
+  const [lastName, setLastName] = useState<string>("");
+  const [userEmail, setUserEmail] = useState<string>("");
+  const [userPassword, setUserPassword] = useState<string>("");
 
   const handleCreate = async () => {
-    const email = "ty@heiprodigital.com";
-    const password = "password";
+    const email = userEmail;
+    const password = userPassword;
 
     try {
       const userCredential = await createUserWithEmailAndPassword(
@@ -25,10 +34,27 @@ const SignUp = () => {
 
       const { user } = userCredential;
 
+      try {
+        await updateProfile(user, {
+          displayName: `${firstName} ${lastName}`,
+        });
+      } catch (error: any) {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+
+        trackError("Update Account", {
+          code: errorCode,
+          message: errorMessage,
+          email,
+          displayName: `${firstName} ${lastName}`,
+        });
+      }
+
       trackIdentity(user.uid, user.email);
       setUser({
         ...user.providerData[0],
         uid: user.uid,
+        displayName: `${firstName} ${lastName}`,
       });
     } catch (error: any) {
       const errorCode = error.code;
@@ -42,14 +68,66 @@ const SignUp = () => {
     }
   };
 
+  const handleName = (e: ChangeEvent<HTMLInputElement>, type: string) => {
+    const val = e.target.value;
+
+    switch (type) {
+      case "firstName":
+        setFirstName(val);
+        break;
+      case "lastName":
+        setLastName(val);
+        break;
+      case "email":
+        setUserEmail(val);
+        break;
+      case "password":
+        setUserPassword(val);
+        break;
+      default:
+        setFirstName(val);
+        break;
+    }
+  };
+
   return (
     <div>
       Please provide us with any feedback. Your feedback will help make our
       product stronger.
-      <div>First Name</div>
-      <div>Last Name</div>
-      <div>Email</div>
-      <div>Password</div>
+      <div>
+        <input
+          type="text"
+          name="firstName"
+          onChange={(e) => handleName(e, "firstName")}
+          placeholder="Enter your first name"
+        />
+      </div>
+      <div>
+        <input
+          type="text"
+          name="lastName"
+          onChange={(e) => handleName(e, "lastName")}
+          placeholder="Enter your last name"
+        />
+      </div>
+      <div>
+        <input
+          type="email"
+          name="email"
+          onChange={(e) => handleName(e, "email")}
+          placeholder="Enter your email"
+          required
+        />
+      </div>
+      <div>
+        <input
+          type="password"
+          name="password"
+          onChange={(e) => handleName(e, "password")}
+          placeholder="Enter your password"
+          required
+        />
+      </div>
       <button onClick={handleCreate}>Create Account</button>
       Already have an account. <Link href="/sign-in">Sign In</Link>
     </div>
