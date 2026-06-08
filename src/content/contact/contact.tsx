a41655b4801fd3eb194570f23a1a2dae5af06194
tyholland <tyronehus@gmail.com>
2026-06-08 07:44:43 -0500

    remove mailtoLink

--- src/content/contact/contact.tsx
+++ src/content/contact/contact.tsx
@@ -13,13 +13,14 @@ const Contact = () => {
   const [lastName, setLastName] = useState<string>("");
   const [userEmail, setUserEmail] = useState<string>("");
   const [feedback, setFeedback] = useState<string>("");
+  const [hasFeedback, setHasFeedback] = useState<boolean>(false);
 
   const handleSubmit = () => {
-    mailtoLink({
-      to: "ty@heiprodigital.com",
-      subject: "BGH Feedback",
-      body: `First Name: ${firstName}\Last Name: ${lastName}\nEmail: ${userEmail}\nMessage: ${feedback}\n`,
-    });
+    // to: "ty@heiprodigital.com",
+    // subject: "BGH Feedback",
+    // body: `First Name: ${firstName}\nLast Name: ${lastName}\nEmail: ${userEmail}\nMessage: ${feedback}\n`,
+
+    setHasFeedback(true);
   };
 
   const handleInputChange = (
@@ -56,45 +57,51 @@ const Contact = () => {
 
   return (
     <S.Wrapper>
-      Please provide us with any feedback. Your feedback will help make our
-      product stronger.
-      <div>
-        <S.Input
-          type="text"
-          name="firstName"
-          onChange={(e) => handleInputChange(e, "firstName")}
-          placeholder="Enter your first name"
-          value={user ? user.displayName?.split(" ")[0] || "" : firstName}
-        />
-      </div>
-      <div>
-        <S.Input
-          type="text"
-          name="lastName"
-          onChange={(e) => handleInputChange(e, "lastName")}
-          placeholder="Enter your last name"
-          value={user ? user.displayName?.split(" ")[1] || "" : lastName}
-        />
-      </div>
-      <div>
-        <S.Input
-          type="email"
-          name="email"
-          onChange={(e) => handleInputChange(e, "email")}
-          placeholder="Enter your email"
-          value={user ? user.email || "" : userEmail}
-          required
-        />
-      </div>
-      <div>
-        <S.Textarea
-          name="feedback"
-          onChange={handleFeedback}
-          placeholder="Enter your feedback"
-          required
-        />
-      </div>
-      <S.Button onClick={handleSubmit}>Submit Feedback</S.Button>
+      {hasFeedback ? (
+        <div>Thanks you for your feedback</div>
+      ) : (
+        <>
+          Please provide us with any feedback. Your feedback will help make our
+          product stronger.
+          <div>
+            <S.Input
+              type="text"
+              name="firstName"
+              onChange={(e) => handleInputChange(e, "firstName")}
+              placeholder="Enter your first name"
+              value={user ? user.displayName?.split(" ")[0] || "" : firstName}
+            />
+          </div>
+          <div>
+            <S.Input
+              type="text"
+              name="lastName"
+              onChange={(e) => handleInputChange(e, "lastName")}
+              placeholder="Enter your last name"
+              value={user ? user.displayName?.split(" ")[1] || "" : lastName}
+            />
+          </div>
+          <div>
+            <S.Input
+              type="email"
+              name="email"
+              onChange={(e) => handleInputChange(e, "email")}
+              placeholder="Enter your email"
+              value={user ? user.email || "" : userEmail}
+              required
+            />
+          </div>
+          <div>
+            <S.Textarea
+              name="feedback"
+              onChange={handleFeedback}
+              placeholder="Enter your feedback"
+              required
+            />
+          </div>
+          <S.Button onClick={handleSubmit}>Submit Feedback</S.Button>
+        </>
+      )}
     </S.Wrapper>
   );
 };
