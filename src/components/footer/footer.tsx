bc952995e27c4eb567df6b2ac36e1364198d9f7d
tyholland <tyronehus@gmail.com>
2026-06-13 23:51:51 -0500

    add footer links

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,18 +1,32 @@
 "use client";
 
 import dayjs from "dayjs";
+import Link from "next/link";
 
 const Footer = () => {
   const year = dayjs().format("YYYY");
 
   return (
     <footer>
-      <div>&copy; {year} BGH Scout.</div>
+      <div className="linkWrapper">
+        <Link href="/about">About</Link>
+        <span>|</span>
+        <Link href="/contact">Contact Us</Link>
+        <span>|</span>
+        <Link href="/request">Request Company</Link>
+        <span>|</span>
+        <Link href="/disclaimer">Disclaimer</Link>
+        <span>|</span>
+        <Link href="/about">Road Map</Link>
+      </div>
       <div>
-        Site by{" "}
-        <a href="https://heiprodigital.com" target="_blank">
-          HeiPro Digital
-        </a>
+        <div>&copy; {year} BGH Scout.</div>
+        <div>
+          Site by{" "}
+          <a href="https://heiprodigital.com" target="_blank">
+            HeiPro Digital
+          </a>
+        </div>
       </div>
     </footer>
   );
