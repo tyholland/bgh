8a31568ec28533beddc6955b2766237f5840cb17
tyholland <tyronehus@gmail.com>
2026-06-26 20:20:29 -0500

    refactor card details

---
+++ src/components/cardDetails-modal/cardDetails-modal.style.ts
@@ -0,0 +1,40 @@
+import styled from "styled-components";
+
+export const ModalWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+`;
+
+export const ModalBtn = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-top: 20px;
+
+  button {
+    width: 100px;
+    padding: 10px;
+    border-radius: 20px;
+    border: none;
+    background: #1439e6;
+
+    @media only screen and (max-width: 950px) {
+      width: 90%;
+    }
+
+    &:hover {
+      cursor: pointer;
+    }
+
+    &.submit {
+      background: #ddd;
+      color: #000;
+
+      &:hover {
+        background: #6faeff;
+        color: #fff;
+      }
+    }
+  }
+`;
---
+++ src/components/cardDetails-modal/cardDetails-modal.tsx
@@ -0,0 +1,65 @@
+"use client";
+
+import * as S from "./cardDetails-modal.style";
+import ModalComponent from "../modal/modal";
+import { CsvData } from "@/types";
+import { trackEvent } from "@/functions/mixpanel";
+import { useAtomValue } from "jotai";
+import { userAtom } from "@/caches/UserAtom";
+
+interface CardDetailsProps {
+  openModal: boolean;
+  setOpenModal: (val: boolean) => void;
+  data: CsvData | null;
+}
+
+const CardDetails = ({ openModal, setOpenModal, data }: CardDetailsProps) => {
+  const user = useAtomValue(userAtom);
+
+  return (
+    <ModalComponent isOpen={openModal} title={`Please Sign In`}>
+      <S.ModalWrapper>
+        <span>Job Details</span>
+        <div>Company: {data?.Company}</div>
+        <div>Industry: {data?.["Primary Industry"]}</div>
+        <div>Role: {data?.["Role Name"]}</div>
+        <div>Date Posted: {data?.Details?.datePosted}</div>
+        <div>Job Description: {data?.Details?.description}</div>
+        <div>Employment Type: {data?.Details?.employmentType}</div>
+        <div>
+          Location: {data?.Details?.jobLocation.address.addressLocality}
+        </div>
+        {data?.Details?.jobBenefits && (
+          <div>Job Benefits: {data?.Details?.jobBenefits}</div>
+        )}
+        {data?.Details?.validThrough && (
+          <div>Valid Through: {data?.Details?.validThrough}</div>
+        )}
+        <S.ModalBtn>
+          <button
+            onClick={() => {
+              trackEvent(user, "See Role", {
+                type: "card",
+                ...data,
+              });
+              setOpenModal(false);
+              window.open(data?.Link);
+            }}
+          >
+            See Role
+          </button>
+          <button
+            onClick={() => {
+              setOpenModal(false);
+            }}
+            className="submit"
+          >
+            Close
+          </button>
+        </S.ModalBtn>
+      </S.ModalWrapper>
+    </ModalComponent>
+  );
+};
+
+export default CardDetails;
