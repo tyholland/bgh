b0b5405e2ee4affc2960066580dcd823fa19b90c
tyholland <tyronehus@gmail.com>
2026-06-27 21:15:17 -0500

    filter modal

---
+++ src/components/filter-modal/filter-modal.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import * as S from "./signIn-modal.style";
+import ModalComponent from "../modal/modal";
+import Filter from "../filter/filter";
+
+interface FilterModalProps {
+  openModal: boolean;
+  setOpenModal: (val: boolean) => void;
+  companies: string[];
+  scrapDates: string[];
+  industries: string[];
+}
+
+const FilterModal = ({
+  openModal,
+  setOpenModal,
+  companies,
+  scrapDates,
+  industries,
+}: FilterModalProps) => {
+  return (
+    <ModalComponent isOpen={openModal} title={`Filter Items`} size="large">
+      <S.ModalWrapper>
+        <Filter
+          companies={companies}
+          scrapDates={scrapDates}
+          industries={industries}
+        />
+        <S.ModalBtn>
+          <button
+            onClick={() => {
+              setOpenModal(false);
+            }}
+          >
+            Close
+          </button>
+        </S.ModalBtn>
+      </S.ModalWrapper>
+    </ModalComponent>
+  );
+};
+
+export default FilterModal;
